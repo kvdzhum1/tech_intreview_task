90651dccfe2c956d61daaa3a257e929a84dd5ed8
kvdzhum1 <kvdzhum1@mts.ru>
2024-10-15 11:11:03 +0700

    ss

--- test_dogs.py
+++ test_dogs.py
@@ -1,27 +1,48 @@
 import random
-
 import pytest
 import requests
 
 
+class URL:
+    BASE_URL_YANDEX = 'https://cloud-api.yandex.net'
+    BASE_URL_BREAD = 'https://dog.ceo/api/breed'
+
+
 class YaUploader:
     def __init__(self):
+        self.token = 'OAuth y0_AgAAAAAEoJcvAADLWwAAAAEUcFM2AADV28-PUSxFx6JhBrgbi2X5wF6y_w'
+        self.base_url = URL()
         pass
 
-    def create_folder(self, path, token):
-        url_create = 'https://cloud-api.yandex.net/v1/disk/resources'
-        headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
-        response = requests.put(f'{url_create}?path={path}', headers = headers)
+    def create_folder(self, path):
+        url_create = f'{self.base_url.BASE_URL_YANDEX}/v1/disk/resources'
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': self.token}
+        requests.put(f'{url_create}?path={path}', headers=headers)
 
-    def upload_photos_to_yd(self, token, path, url_file, name):
-        url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-        headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
-        params = {"path": f'/{path}/{name}', 'url': url_file, "overwrite": "true"}
-        resp = requests.post(url, headers=headers, params=params)
+    def upload_photos_to_yd(self, path, url_file, name):
+        url = f"{self.base_url.BASE_URL_YANDEX}/v1/disk/resources/upload"
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': self.token}
+        params = {"path": f'/{path}/{name}', 'url': url_file}
+        requests.post(url, headers=headers, params=params)
+
+    def get_folder(self):
+        url_create = f'{self.base_url.BASE_URL_YANDEX}/v1/disk/resources'
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json',
+                   'Authorization': self.token}
+        return requests.delete(f'{url_create}?path=/test_folder', headers=headers)
+
+    def delete_folder(self, path):
+        url = f'{self.base_url.BASE_URL_YANDEX}/v1/disk/resources'
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json',
+                   'Authorization': self.token}
+        requests.get(f'{url}?path=/{path}', headers=headers)
+
+
+base_url = URL()
 
 
 def get_sub_breeds(breed):
-    res = requests.get(f'https://dog.ceo/api/breed/{breed}/list')
+    res = requests.get(f'{base_url.BASE_URL_BREAD}/{breed}/list')
     return res.json().get('message', [])
 
 
@@ -29,11 +50,11 @@ def get_urls(breed, sub_breeds):
     url_images = []
     if sub_breeds:
         for sub_breed in sub_breeds:
-            res = requests.get(f"https://dog.ceo/api/breed/{breed}/{sub_breed}/images/random")
+            res = requests.get(f"{base_url.BASE_URL_BREAD}/{breed}/{sub_breed}/images/random")
             sub_breed_urls = res.json().get('message')
             url_images.append(sub_breed_urls)
     else:
-        url_images.append(requests.get(f"https://dog.ceo/api/breed/{breed}/images/random").json().get('message'))
+        url_images.append(requests.get(f"{base_url.BASE_URL_BREAD}/{breed}/images/random").json().get('message'))
     return url_images
 
 
@@ -41,24 +62,24 @@ def u(breed):
     sub_breeds = get_sub_breeds(breed)
     urls = get_urls(breed, sub_breeds)
     yandex_client = YaUploader()
-    yandex_client.create_folder('test_folder', "AgAAAAAJtest_tokenxkUEdew")
+    yandex_client.delete_folder('test_folder')
+    yandex_client.create_folder('test_folder')
     for url in urls:
         part_name = url.split('/')
         name = '_'.join([part_name[-2], part_name[-1]])
-        yandex_client.upload_photos_to_yd("AgAAAAAJtest_tokenxkUEdew", "test_folder", url, name)
+        yandex_client.upload_photos_to_yd("test_folder", url, name)
 
 
-@pytest.mark.parametrize('breed', ['doberman', random.choice(['bulldog', 'collie'])])
+@pytest.mark.parametrize('breed', ['doberman', random.choice(['bulldog', 'spaniel'])])
 def test_proverka_upload_dog(breed):
     u(breed)
+    yandex_client = YaUploader()
     # проверка
-    url_create = 'https://cloud-api.yandex.net/v1/disk/resources'
-    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth AgAAAAAJtest_tokenxkUEdew'}
-    response = requests.get(f'{url_create}?path=/test_folder', headers=headers)
-    assert response.json()['type'] == "dir"
+    response = yandex_client.get_folder()
+    assert response.status_code == 200, f'Ошибка! Код ответа {response.status_code}. Тело ответа: {response.json()}'
+    assert response.json() == "dir"
     assert response.json()['name'] == "test_folder"
-    assert True
-    if get_sub_breeds(breed) == []:
+    if not get_sub_breeds(breed):
         assert len(response.json()['_embedded']['items']) == 1
         for item in response.json()['_embedded']['items']:
             assert item['type'] == 'file'
@@ -69,5 +90,3 @@ def test_proverka_upload_dog(breed):
         for item in response.json()['_embedded']['items']:
             assert item['type'] == 'file'
             assert item['name'].startswith(breed)
-
-
